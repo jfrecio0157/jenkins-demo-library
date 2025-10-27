5ee2a6622cc61f44cc4e65316105e5a61f97baba
Paco Recio <jfrecio@gmail.com>
2025-10-27 18:09:50 +0100

    Cambios en el uso de la variable mail

--- vars/sendMailFailure.groovy
+++ vars/sendMailFailure.groovy
@@ -1,7 +1,7 @@
 def call(Map config){
     node{
         echo "Envio correo Failure"
-        mail to: ${config.mail},
+        mail to: config.mail,
                 subject: "Build Fallida: ${env.JOB_NAME} #${env.BUILD_NUMBER}",
                 body: "La build ha fallado. Revisa Jenkins en ${env.BUILD_URL}"
     }
